437f25660be20b72cc5035361c53042e65b92e87
mrashed21 <rashedjaman768@gmail.com>
2025-12-04 23:44:56 +0600

    setup vehicle rentel system server

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 5000;
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "server is running",
+    path: req.path,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Vehicle Rental System server running at ${PORT}`);
+});
